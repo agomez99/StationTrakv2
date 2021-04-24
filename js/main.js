410c814c4df20004112910376799d8c7e712fcea
Austine Gomez <austine.gomez@yahoo.com>
2021-04-24 15:21:22 -0500

    updated list of people with api

--- js/main.js
+++ js/main.js
@@ -1,253 +1,253 @@
-// Main JS COde here.
-$(document).ready(function() {
-  // map is reponsive. requires a page load will not re-size with a window size change
-  var width = $("#map").width() * 0.85,
-    height = width / 2;
+// // Main JS COde here.
+// $(document).ready(function() {
+//   // map is reponsive. requires a page load will not re-size with a window size change
+//   var width = $("#map").width() * 0.85,
+//     height = width / 2;
 
-   var svg = d3
-    .select("#map")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+//    var svg = d3
+//     .select("#map")
+//     .append("svg")
+//     .attr("width", width)
+//     .attr("height", height);
 
-  var projection = d3.geo
-    .equirectangular()
-    .scale(width / 1.89 / Math.PI)
-    .translate([width / 2, height / 2]);
+//   var projection = d3.geo
+//     .equirectangular()
+//     .scale(width / 1.89 / Math.PI)
+//     .translate([width / 2, height / 2]);
 
-  var path = d3.geo.path().projection(projection);
+//   var path = d3.geo.path().projection(projection);
 
-  var graticule = d3.geo.graticule();
+//   var graticule = d3.geo.graticule();
 
-  var imgTransformFactor = -(width * 0.1) / 2;
+//   var imgTransformFactor = -(width * 0.1) / 2;
 
-  d3.json("js/world-110m.json", function(error, world) {
-    svg
-      .style("background-color", "#1d87c5")
-      .style("border", "1px solid black")
-      .append("g")
-      .attr("class", "land")
-      .selectAll("path")
-      .data([topojson.object(world, world.objects.land)])
-      .enter()
-      .append("path")
-      .attr("d", path)
-      .attr("fill", "#1c8f4c");
-    svg
-      .append("g")
-      .attr("class", "boundary")
-      .selectAll("boundary")
-      .data([topojson.object(world, world.objects.countries)])
-      .enter()
-      .append("path")
-      .attr("fill", "#1c8f4c")
-      .attr("stroke", "#766951")
-      .attr("d", path);
-    svg
-      .append("g")
-      .attr("class", "graticule")
-      .selectAll("path")
-      .data(graticule.lines)
-      .enter()
-      .append("path")
-      .attr("d", path)
-      .attr("stroke", "grey")
-      .attr("opacity", "0.5");
+//   d3.json("js/world-110m.json", function(error, world) {
+//     svg
+//       .style("background-color", "#1d87c5")
+//       .style("border", "1px solid black")
+//       .append("g")
+//       .attr("class", "land")
+//       .selectAll("path")
+//       .data([topojson.object(world, world.objects.land)])
+//       .enter()
+//       .append("path")
+//       .attr("d", path)
+//       .attr("fill", "#1c8f4c");
+//     svg
+//       .append("g")
+//       .attr("class", "boundary")
+//       .selectAll("boundary")
+//       .data([topojson.object(world, world.objects.countries)])
+//       .enter()
+//       .append("path")
+//       .attr("fill", "#1c8f4c")
+//       .attr("stroke", "#766951")
+//       .attr("d", path);
+//     svg
+//       .append("g")
+//       .attr("class", "graticule")
+//       .selectAll("path")
+//       .data(graticule.lines)
+//       .enter()
+//       .append("path")
+//       .attr("d", path)
+//       .attr("stroke", "grey")
+//       .attr("opacity", "0.5");
 
-    getCurrentStationPosition();
-    getCurrentStationPosition2()
-    getCurrentStationPosition3()
-    getCurrentStationPosition4()
+//     getCurrentStationPosition();
+//     getCurrentStationPosition2()
+//     getCurrentStationPosition3()
+//     getCurrentStationPosition4()
 
-  });
+//   });
 
   
  
-//NOAA19
-  function getCurrentStationPosition() {
-    //console.log("Getting Station");
-    fetch(
-      "https://api.n2yo.com/rest/v1/satellite/positions/33591/29.5891833/-98.6270735/0/1/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII", {
-        mode: 'no-cors', // no-cors, *cors, same-origin
+// //NOAA19
+//   function getCurrentStationPosition() {
+//     //console.log("Getting Station");
+//     fetch(
+//       "https://api.n2yo.com/rest/v1/satellite/positions/33591/29.5891833/-98.6270735/0/1/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII", {
+//         mode: 'no-cors', // no-cors, *cors, same-origin
 
-      }
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        let coord = projection([
-          data.positions[0].satlongitude,
-          data.positions[0].satlatitude
-        ]);
+//       }
+//     )
+//       .then(response => {
+//         return response.json();
+//       })
+//       .then(data => {
+//         let coord = projection([
+//           data.positions[0].satlongitude,
+//           data.positions[0].satlatitude
+//         ]);
 
-        svg
-          .append("image")
-          .attr("x", coord[0])
-          .attr("y", coord[1])
-          .attr("href", "images/NOAA19.png")
+//         svg
+//           .append("image")
+//           .attr("x", coord[0])
+//           .attr("y", coord[1])
+//           .attr("href", "images/NOAA19.png")
 
-          .attr("height", width * 0.1)
-          .attr("width", width * 0.1)
-          .attr(
-            "transform",
-            "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
-          );
-          svg
-          .append("image")
-          .attr("x", coord[0])
-          .attr("y", coord[1])
-          .attr("href", "img/marker.png")
-          .attr("height", width * 0.03)
-          .attr("width", width * 0.1)
-          .attr(
-            "transform",
-            "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
-          );
-      });
-  }
+//           .attr("height", width * 0.1)
+//           .attr("width", width * 0.1)
+//           .attr(
+//             "transform",
+//             "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
+//           );
+//           svg
+//           .append("image")
+//           .attr("x", coord[0])
+//           .attr("y", coord[1])
+//           .attr("href", "img/marker.png")
+//           .attr("height", width * 0.03)
+//           .attr("width", width * 0.1)
+//           .attr(
+//             "transform",
+//             "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
+//           );
+//       });
+//   }
 
-//GOES13
-  function getCurrentStationPosition2() {
-    //console.log("Getting Station");
-    fetch(
-      "https://api.n2yo.com/rest/v1/satellite/positions/29155/29.5891833/-98.6270735/0/1/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII",{
-        mode: 'no-cors', // no-cors, *cors, same-origin
+// //GOES13
+//   function getCurrentStationPosition2() {
+//     //console.log("Getting Station");
+//     fetch(
+//       "https://api.n2yo.com/rest/v1/satellite/positions/29155/29.5891833/-98.6270735/0/1/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII",{
+//         mode: 'no-cors', // no-cors, *cors, same-origin
 
-      }
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        let coord = projection([
-          data.positions[0].satlongitude,
-          data.positions[0].satlatitude
-        ]);
+//       }
+//     )
+//       .then(response => {
+//         return response.json();
+//       })
+//       .then(data => {
+//         let coord = projection([
+//           data.positions[0].satlongitude,
+//           data.positions[0].satlatitude
+//         ]);
         
-        svg
-          .append("image")
-          .attr("x", coord[0])
-          .attr("y", coord[1])
-          .attr("href", "images/AQUA.png")
-          .attr("height", width * 0.1)
-          .attr("width", width * 0.1)
-          .attr(
-            "transform",
-            "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
-          );
-          svg
-          .append("image")
-          .attr("x", coord[0])
-          .attr("y", coord[1])
-          .attr("href", "img/marker2.png")
-          .attr("height", width * 0.03)
-          .attr("width", width * 0.1)
-          .attr(
-            "transform",
-            "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
-          );
-      });
+//         svg
+//           .append("image")
+//           .attr("x", coord[0])
+//           .attr("y", coord[1])
+//           .attr("href", "images/AQUA.png")
+//           .attr("height", width * 0.1)
+//           .attr("width", width * 0.1)
+//           .attr(
+//             "transform",
+//             "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
+//           );
+//           svg
+//           .append("image")
+//           .attr("x", coord[0])
+//           .attr("y", coord[1])
+//           .attr("href", "img/marker2.png")
+//           .attr("height", width * 0.03)
+//           .attr("width", width * 0.1)
+//           .attr(
+//             "transform",
+//             "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
+//           );
+//       });
 
 
 
-  }
+//   }
 
-//SES1
-  function getCurrentStationPosition3() {
-    //console.log("Getting Station");
-    fetch( "https://api.n2yo.com/rest/v1/satellite/positions/36516/29.5891833/-98.6270735/0/1/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII",{
-      mode: 'no-cors', // no-cors, *cors, same-origin
+// //SES1
+//   function getCurrentStationPosition3() {
+//     //console.log("Getting Station");
+//     fetch( "https://api.n2yo.com/rest/v1/satellite/positions/36516/29.5891833/-98.6270735/0/1/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII",{
+//       mode: 'no-cors', // no-cors, *cors, same-origin
 
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        let coord = projection([
-          data.positions[0].satlongitude,
-          data.positions[0].satlatitude
-        ]);
-
-
-
-        svg
-          .append("image")
-          .attr("x", coord[0])
-          .attr("y", coord[1])
-          .attr("href", "images/SES1.jpg")
-          .attr("height", width * 0.1)
-          .attr("width", width * 0.1)
-          .attr(
-            "transform",
-            "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
-          );
-          svg
-          .append("image")
-          .attr("x", coord[0])
-          .attr("y", coord[1])
-          .attr("href", "img/marker3.png")
-          .attr("height", width * 0.03)
-          .attr("width", width * 0.1)
-          .attr(
-            "transform",
-            "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
-          );
-      });
-  }
+//     })
+//       .then(response => {
+//         return response.json();
+//       })
+//       .then(data => {
+//         let coord = projection([
+//           data.positions[0].satlongitude,
+//           data.positions[0].satlatitude
+//         ]);
 
 
 
-//TERRA
-  function getCurrentStationPosition4() {
-    //console.log("Getting Station");
-    fetch(
-      "https://api.n2yo.com/rest/v1/satellite/positions/25994/29.5891833/-98.6270735/0/1/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII",
-      {
-        mode: 'no-cors', // no-cors, *cors, same-origin
-
-      }
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        let coord = projection([
-          data.positions[0].satlongitude,
-          data.positions[0].satlatitude
-        ]);
-
-
-
-        svg
-          .append("image")
-          .attr("x", coord[0])
-          .attr("y", coord[1])
-          .attr("href", "images/TERRA.png")
-          .attr("height", width * 0.1)
-          .attr("width", width * 0.1)
-          .attr(
-            "transform",
-            "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
-          );
-          svg
-          .append("image")
-          .attr("x", coord[0])
-          .attr("y", coord[1])
-          .attr("href", "img/marker4.png")
-          .attr("height", width * 0.03)
-          .attr("width", width * 0.1)
-          .attr(
-            "transform",
-            "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
-          );
-      });
-  }
+//         svg
+//           .append("image")
+//           .attr("x", coord[0])
+//           .attr("y", coord[1])
+//           .attr("href", "images/SES1.jpg")
+//           .attr("height", width * 0.1)
+//           .attr("width", width * 0.1)
+//           .attr(
+//             "transform",
+//             "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
+//           );
+//           svg
+//           .append("image")
+//           .attr("x", coord[0])
+//           .attr("y", coord[1])
+//           .attr("href", "img/marker3.png")
+//           .attr("height", width * 0.03)
+//           .attr("width", width * 0.1)
+//           .attr(
+//             "transform",
+//             "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
+//           );
+//       });
+//   }
 
 
 
+// //TERRA
+//   function getCurrentStationPosition4() {
+//     //console.log("Getting Station");
+//     fetch(
+//       "https://api.n2yo.com/rest/v1/satellite/positions/25994/29.5891833/-98.6270735/0/1/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII",
+//       {
+//         mode: 'no-cors', // no-cors, *cors, same-origin
+
+//       }
+//     )
+//       .then(response => {
+//         return response.json();
+//       })
+//       .then(data => {
+//         let coord = projection([
+//           data.positions[0].satlongitude,
+//           data.positions[0].satlatitude
+//         ]);
 
 
-});
+
+//         svg
+//           .append("image")
+//           .attr("x", coord[0])
+//           .attr("y", coord[1])
+//           .attr("href", "images/TERRA.png")
+//           .attr("height", width * 0.1)
+//           .attr("width", width * 0.1)
+//           .attr(
+//             "transform",
+//             "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
+//           );
+//           svg
+//           .append("image")
+//           .attr("x", coord[0])
+//           .attr("y", coord[1])
+//           .attr("href", "img/marker4.png")
+//           .attr("height", width * 0.03)
+//           .attr("width", width * 0.1)
+//           .attr(
+//             "transform",
+//             "translate(" + imgTransformFactor + "," + imgTransformFactor + ")"
+//           );
+//       });
+//   }
+
+
+
+
+
+// });
 
 
--- js/script.js
+++ js/script.js
@@ -1,66 +1,110 @@
 
 
 $(document).ready(function () {
+
+
+
+const spacePeople = () =>{
+  return new Promise((resolves, rejects) => {
+    const api = 
+    "http://api.open-notify.org/astros.json";
+    const request = new XMLHttpRequest();
+    request.open("GET", api);
+    request.onload = () => {
+      if (request.status === 200){
+        resolves(JSON.parse(request.response));
+      }else{
+        rejects(Error(request.statusText));
+      }
+      };
+      request.onerror = (err) => rejects(err);
+      request.send();
+    
+  });
+}
+
+spacePeople().then((spaceData) =>
+  console.log(spaceData)
+  
+);
+
+
+$.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
+    var number = data['number'];
+    $('#spacepeeps').html(number);
+
+    data['people'].forEach(function (d) {
+         $('#astronames').append('<li>' + d['name'] + '</li>');
+    });
+});
+
+
+
+
+
+
+
+
   //TODAY APOD
-  var space2 =
-    "https://api.nasa.gov/planetary/apod?api_key=RfKWkoYrn7N6w0tIX57JIbQafgqh3fV2Oo4zCbfd";
+  // var space2 =
+  //   "https://api.nasa.gov/planetary/apod?api_key=RfKWkoYrn7N6w0tIX57JIbQafgqh3fV2Oo4zCbfd";
 
-  //NOAA19
-  const NOAA19 = "33591";
-  //AQUA
-  const GOES13 = "29155";
-  //SES1
-  const SES1 = "36516";
-  //TERRA
-  const TERRA = "25994";
+  // //NOAA19
+  // const NOAA19 = "33591";
+  // //AQUA
+  // const GOES13 = "29155";
+  // //SES1
+  // const SES1 = "36516";
+  // //TERRA
+  // const TERRA = "25994";
 
 
-  let satName1 = NOAA19;
-  let satName2 = GOES13;
-  let satName3 = SES1;
-  let satName4 = TERRA;
+  // let satName1 = NOAA19;
+  // let satName2 = GOES13;
+  // let satName3 = SES1;
+  // let satName4 = TERRA;
 
-  let getCoords = "https://api.n2yo.com/rest/v1/satellite/positions/" + satName1 + "/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII";
-  let getCoords2 = "https://api.n2yo.com/rest/v1/satellite/positions/" + satName2 + "/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII";
-  let getCoords3 = "https://api.n2yo.com/rest/v1/satellite/positions/" + satName3 + "/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII";
-  let getCoords4 = "https://api.n2yo.com/rest/v1/satellite/positions/" + satName4 + "/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII";
+  // let getCoords = "https://api.n2yo.com/rest/v1/satellite/positions/" + satName1 + "/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII";
+  // let getCoords2 = "https://api.n2yo.com/rest/v1/satellite/positions/" + satName2 + "/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII";
+  // let getCoords3 = "https://api.n2yo.com/rest/v1/satellite/positions/" + satName3 + "/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII";
+  // let getCoords4 = "https://api.n2yo.com/rest/v1/satellite/positions/" + satName4 + "/41.702/-76.014/0/300/&apiKey=AFQ4CY-H89EGX-EFBHPT-4BII";
 
-  var cords = getCoords;
-  var cords2 = getCoords2;
-  var cords3 = getCoords3;
-  var cords4 = getCoords4;
+  // var cords = getCoords;
+  // var cords2 = getCoords2;
+  // var cords3 = getCoords3;
+  // var cords4 = getCoords4;
 
   var d = new Date();
   document.getElementById("currentT").innerHTML = d;
 
 
-  //APOD
-  $.ajax({
-    url: space2,
-    method: "GET",
-    mode: "no-cors",
-  }).then(function (response) {
-    $("#spaceTitle").html(response.title);
-    $("#Sdate").html(response.date);
-    $("#info").html(response.explanation);
-    $("#spaceImg").attr("src", response.url);
-  });
+  // //APOD
+  // $.ajax({
+  //   url: space2,
+  //   method: "GET",
+  //   mode: "no-cors",
+  // }).then(function (response) {
+  //   $("#spaceTitle").html(response.title);
+  //   $("#Sdate").html(response.date);
+  //   $("#info").html(response.explanation);
+  //   $("#spaceImg").attr("src", response.url);
+  // });
 
 
-  $.ajax({
-    url: cords,
-    method: "GET",
-    mode: "no-cors",
+  // $.ajax({
+  //   url: cords,
+  //   method: "GET",
+  //   mode: "no-cors",
 
-  }).then(function (response) {
-    $("#satName").html(response.info.satname);
-    for (i in response.positions) {
-      y = response.positions[i].satlatitude;
-      x = response.positions[i].satlongitude;
-    }
-    $("#positionx").html("latitude: " + x)
-    $("#positiony").html("longitude: " + y)
-    $("#alt").html("Altitude: " + response.positions[2].sataltitude)
+  // }).then(function (response) {
+  //   $("#satName").html(response.info.satname);
+  //   for (i in response.positions) {
+  //     y = response.positions[i].satlatitude;
+  //     x = response.positions[i].satlongitude;
+  //   }
+  //   $("#positionx").html("latitude: " + x)
+  //   $("#positiony").html("longitude: " + y)
+  //   $("#alt").html("Altitude: " + response.positions[2].sataltitude)
 
     // var times = response.positions[0].timestamp;
     // var ts = times;
@@ -88,53 +132,56 @@ $(document).ready(function () {
     // );
   });
 
-  $.ajax({
-    url: cords2,
-    method: "GET",
-  }).then(function (response) {
-    $("#satName2").html(response.info.satname);
-    for (i in response.positions) {
-      y = response.positions[i].satlatitude;
-      x = response.positions[i].satlongitude;
-    }
-    $("#positionx2").html("latitude: " + x)
-    $("#positiony2").html("longitude: " + y)
-    $("#alt2").html("Altitude: " + response.positions[2].sataltitude)
+  // $.ajax({
+  //   url: cords2,
+  //   method: "GET",
+  // }).then(function (response) {
+  //   $("#satName2").html(response.info.satname);
+  //   for (i in response.positions) {
+  //     y = response.positions[i].satlatitude;
+  //     x = response.positions[i].satlongitude;
+  //   }
+  //   $("#positionx2").html("latitude: " + x)
+  //   $("#positiony2").html("longitude: " + y)
+  //   $("#alt2").html("Altitude: " + response.positions[2].sataltitude)
 
-  });
-  $.ajax({
-    url: cords3,
-    method: "GET",
-  }).then(function (response) {
-    $("#satName3").html(response.info.satname);
-    for (i in response.positions) {
-      y = response.positions[i].satlatitude;
-      x = response.positions[i].satlongitude;
-    }
-    $("#positionx3").html("latitude: " + x)
-    $("#positiony3").html("longitude: " + y)
-    $("#alt3").html("Altitude: " + response.positions[2].sataltitude)
+  // });
+  // $.ajax({
+  //   url: cords3,
+  //   method: "GET",
+  // }).then(function (response) {
+  //   $("#satName3").html(response.info.satname);
+  //   for (i in response.positions) {
+  //     y = response.positions[i].satlatitude;
+  //     x = response.positions[i].satlongitude;
+  //   }
+  //   $("#positionx3").html("latitude: " + x)
+  //   $("#positiony3").html("longitude: " + y)
+  //   $("#alt3").html("Altitude: " + response.positions[2].sataltitude)
 
-  });
-
-
-  $.ajax({
-    url: cords4,
-    method: "GET",
-  }).then(function (response) {
-    $("#satName4").html(response.info.satname);
-    for (i in response.positions) {
-      y = response.positions[i].satlatitude;
-      x = response.positions[i].satlongitude;
-    }
-    $("#positionx4").html("latitude: " + x)
-    $("#positiony4").html("longitude: " + y)
-    $("#alt4").html("Altitude: " + response.positions[2].sataltitude)
-
-  });
+  // });
 
 
-});
+  // $.ajax({
+  //   url: cords4,
+  //   method: "GET",
+  // }).then(function (response) {
+  //   $("#satName4").html(response.info.satname);
+  //   for (i in response.positions) {
+  //     y = response.positions[i].satlatitude;
+  //     x = response.positions[i].satlongitude;
+  //   }
+  //   $("#positionx4").html("latitude: " + x)
+  //   $("#positiony4").html("longitude: " + y)
+  //   $("#alt4").html("Altitude: " + response.positions[2].sataltitude)
+
+  // });
+
+
+
+
+
+
 
 
 
